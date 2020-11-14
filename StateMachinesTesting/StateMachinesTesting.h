f7abe1f0059849419a7901ef10d0cf1e874559f4
Viacheslav Volodko <killobatt@gmail.com>
2020-11-14 20:52:35 +0200

    Initial Commit

---
+++ StateMachinesTesting/StateMachinesTesting.h
@@ -0,0 +1,18 @@
+//
+//  StateMachinesTesting.h
+//  StateMachinesTesting
+//
+//  Created by Viacheslav Volodko on 14.11.2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StateMachinesTesting.
+FOUNDATION_EXPORT double StateMachinesTestingVersionNumber;
+
+//! Project version string for StateMachinesTesting.
+FOUNDATION_EXPORT const unsigned char StateMachinesTestingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StateMachinesTesting/PublicHeader.h>
+
+
